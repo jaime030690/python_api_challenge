9b397d58fc38d2539a031d39c2a7533282ed8e6c
Jaime Echevarria <jechvarria@cgsinc.com>
2020-01-24 09:08:23 -0500

    Completed test code for creating DF with weather data.

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "aca65d18dec8794e406033e10d604a34"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyABCEV7of9gv-QhJZJcGFP6gd4DKzO6UJc"
